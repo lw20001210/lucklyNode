1468a50c72f64e7940ee33022477a66cdbe83cdb
威哥 <2972063787@qq.com>
2023-05-04 14:37:10 +0800

    完成了注册功能

--- router/users.js
+++ router/users.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const db = require("../mysql/userSql");
+const bcrypt = require("bcryptjs");
 var multipart = require("connect-multiparty");
 // 2.创建路由对象
 const router = express.Router();
 router.post("/register", multipart(), (req, res) => {
   console.log(req.body);
+  console.log(req.files);
+  if (!req.files.avatar.path) {
+    return res.send({
+      msg: "注册失败",
+      code: 500,
+    });
+  }
+  let newObj = {
+    ...req.body,
+    password: bcrypt.hashSync(req.body.password, 10),
+    avatar: req.files.avatar.path,
+  };
   const sqlStr = "insert into users set ?";
-  db.query(sqlStr, req.body, (err, data) => {
+  db.query(sqlStr, newObj, (err, data) => {
     if (err) {
-      console.log(err.message);
+      // console.log(err.message);
       if (err.message.includes("for key 'users.username'")) {
         res.send({
           msg: "该用户已被注册",
@@ -24,8 +37,11 @@ router.post("/register", multipart(), (req, res) => {
       res.send({
         msg: "注册成功",
         code: 200,
+        data: data,
+        obj: newObj,
       });
     }
   });
 });
+router.post("/login", (req, res) => {});
 module.exports = router;
