a495796cda5feb76606065626e72ac91d37e7a94
linwei <135044867+lw20001210@users.noreply.github.com>
2024-05-25 18:09:49 +0800

    把远程数据库改为本地

--- mysql/sequlize.js
+++ mysql/sequlize.js
@@ -3,7 +3,8 @@ const Sequelize = require("sequelize");
 // 参数1：数据库名，参数2：用户名，参数3：密码。{参数1：数据库主机地址，参数2：数据库类型，参数3：是否打印日志}
 const sequelize = new Sequelize("mychatapp", "root", "admin123", {
   host: "127.0.0.1",
-  port:"3307",
+  // port:"3307",
+  port:"3306",
   dialect: "mysql",
   logging: false,
 });
